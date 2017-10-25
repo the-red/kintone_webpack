d9fb980712f417af27ae73495346a2952a1903b0
Hisaki Akaza <TheRed@users.noreply.github.com>
2017-10-25 12:14:57 +0900

    app1 kintoneUtilityでレコードを1000件追加する処理を実装

--- src/app1.js
+++ src/app1.js
@@ -1,9 +1,35 @@
-import TheWorld from './TheWorld';
+import $ from 'jquery';
+import kintoneUtility from 'kintone-utility/docs/kintoneUtility';
+
+const insertRecord = async () => {
+  // レコード取得
+  const { getRecords } = await kintoneUtility.rest.getRecords({
+    app: kintone.app.getId(),
+    query: kintone.app.getQuery(),
+  });
+  console.log(getRecords);
+
+  // レコード挿入
+  const postRecords = [];
+  for (let i = 0; i < 1000; i++) {
+    const r = {
+      予算: { value: i },
+      拠点: { value: `拠点${i}` },
+    };
+    postRecords.push(r);
+  }
+  const postResp = await kintoneUtility.rest.postRecords({
+    app: kintone.app.getId(),
+    records: postRecords,
+  });
+  console.log(postResp);
+};
 
 kintone.events.on('app.record.index.show', () => {
-  new TheWorld(3).run();
-});
+  const $button = $('<button>', {
+    text: 'レコードを1000件追加',
+    click: insertRecord,
+  });
 
-kintone.events.on('app.record.detail.show', () => {
-  new TheWorld(1).run();
+  $(kintone.app.getHeaderMenuSpaceElement()).append($button);
 });
